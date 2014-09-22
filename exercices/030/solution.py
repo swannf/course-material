66ecef2ee55dc3421364f118ea49f4f9f5095014
swannf <swann.flochlay@gmail.com>
2014-09-22 16:33:43 +0200

    new update

--- exercices/030/solution.py
+++ exercices/030/solution.py
@@ -1,3 +1,3 @@
-for n in range(0,101):
-    if n%2==0:
+for n in range(0, 101):
+    if n % 2 == 0:
         print(n)
--- exercices/070/solution.py
+++ exercices/070/solution.py
@@ -1,6 +1,6 @@
-alpha='abcdefghijklmnopqrstuvwxyz'
-siz=len(alpha)
-for i in range(0,siz):
-    for j in range(0,siz):
-        if alpha[i]!=alpha[j]:
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+siz = len(alpha)
+for i in range(0, siz):
+    for j in range(0, siz):
+        if alpha[i] != alpha[j]:
             print(alpha[i]+alpha[j])
