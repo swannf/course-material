8e312db2873eb72e089ecd8e5b5762f3d2e344ba
swannf <swann.flochlay@gmail.com>
2014-09-23 15:48:34 +0200

    upd d2aftnn 3

--- exercices/201/solution.py
+++ exercices/201/solution.py
@@ -1,9 +1,9 @@
 def is_alpha(input):
     input = input.lower()
     letter = 'abcdefghijklmnopqrstuvwyxz'
-    resp = False
-    for let in letter:
-        if let in input:
-            resp = True
+    resp = True
+    for let in input:
+        if let not in letter:
+            resp = False
             break
     return resp
