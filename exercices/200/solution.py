84f9501bc37d6119852173af1f7e3b30c84ed6db
swannf <swann.flochlay@gmail.com>
2014-09-23 12:14:02 +0200

    last d2 morning

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -1,6 +1,6 @@
 def is_prime(num):
     div = []
-    for i in range(int((num ** 0.5)+1), 2, -1):
+    for i in range(2, int((num ** 0.5)+1)):
         if num % i == 0:
             div.append(i)
             break
